c267f743f88104742f598fb4d765140dead1e39c
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-02-27 00:21:17 +0500

    Add files via upload

---
+++ Q12.js
@@ -0,0 +1,7 @@
+// 12. Greeting start with the array you used in Exercise 11, but intead of just printing each persons name, print a message of them . The text of each message should be the state, out each message should be personalized with the persons name .
+var names = ["Aliza:", "Arfa:", "Tooba:", "Mishal:",];
+var message = "You are lucky more than you think, just believe in yourself everything will come to you in the right time";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
